a3fcc12f2a2c6408262d142bba500843ca420480
Jaffa Aharonov <jaharonov@gmail.com>
2018-11-10 11:54:29 -0600

    got the nesting for modals to work

--- src/components/Faculty.vue
+++ src/components/Faculty.vue
@@ -15,13 +15,14 @@
       
     
   <!-- Using modifiers -->
-  <b-img v-b-modal.modal @click="onShowModal = true" :src="image.src" :alt="image.alt" fluid-grow />
+  <b-img v-b-modal="image.name" :src="image.src" :alt="image.alt" fluid-grow />
 
   <!-- Using value -->
 
   <!-- the modal -->
-  <b-modal v-if="onShowModal" id="modal">
+  <b-modal :id="image.name">
     <b-img :src="image.src" fluid />
+    
     <p class="text-justify">{{image.bio1}}</p>
     <p class="text-justify">{{image.bio2}}</p>
     <p class="text-justify">{{image.bio3}}</p>
@@ -37,13 +38,13 @@
 
 <script>
 export default {
-  name: 'modal',
+  name: '',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App',
       images: [
         // { src: './static/Old-Main-WSU.jpg', alt: 'Old Main building Wayne State University' },
-        { id: 'denise_castion',
+        { name: 'modal1',
+          id: 'denise_castion',
           src: './static/faculty_denise-caston.jpg',
           alt: 'Music Hall',
           bio1: 'Denise Caston was a Radio City Rockette for 10 years and toured nationally with the Tony Award winning musical, Crazy For You. Denise has also had the honor of performing the choreography of Savion Glover at a showcase in New York City. She performed on tour in Australia, dancing all over the continent in Elvis to the Max. Her choreography has been seen in many productions in which she performed, including shows at Caesars Windsor in Ontario, Canada. Credits also include film, television, commercials, and industrials.',
@@ -51,20 +52,15 @@ export default {
           bio3: 'In 2011, Denise was invited to speak at the TEDx Detroit Conference about her contributions to the cultural landscape in Detroit, including founding and producing Motor City Tap Fest and founding the Detroit Tap Repertory.',
           bio4: 'In 2014, she was awarded a prestigious MaTilDa Award from her alma mater, Oakland University, for Alumni Achievement in Dance.'
         },
-        { id: 'claudia',
+        { name: 'modal2',
+          id: 'claudia',
           src: './static/faculty_claudia-rahardjanoto.jpg',
           alt: 'Music Hall',
           bio1: 'Denise Caston was a Radio City Rockette for 10 years and toured nationally with the Tony Award winning musical, Crazy For You. Denise has also had the honor of performing the choreography of Savion Glover at a showcase in New York City. She performed on tour in Australia, dancing all over the continent in Elvis to the Max. Her choreography has been seen in many productions in which she performed, including shows at Caesars Windsor in Ontario, Canada. Credits also include film, television, commercials, and industrials.',
           bio2: 'She currently teaches at Broadway Dance Center and Steps on Broadway. Other teaching credits include Beyond The Stars Dance Convention, Phoenix Tap Fest, OKC Tap Fest, Gregg Russell\'s Tap Into the Network, Tradition In Tap, and The Rockette Experience© at Radio City Music Hall.',
           bio3: 'In 2011, Denise was invited to speak at the TEDx Detroit Conference about her contributions to the cultural landscape in Detroit, including founding and producing Motor City Tap Fest and founding the Detroit Tap Repertory.',
           bio4: 'In 2014, she was awarded a prestigious MaTilDa Award from her alma mater, Oakland University, for Alumni Achievement in Dance.'
-        }],
-      showModal: false
-    }
-  },
-  methods: {
-    onShowModal () {
-      this.showModal = true
+        }]
     }
   }
 }
