c78e9f299f205d46dadf3068f2a13f59421ec430
Jaffa Aharonov <jaharonov@gmail.com>
2018-12-07 14:06:27 -0600

    changed scholarships page format

--- src/components/Scholarships.vue
+++ src/components/Scholarships.vue
@@ -10,19 +10,15 @@
             <b-img class="p-2" center src="./static/scholarships1.jpg" fluid/>     
             <b-img class="p-2" center src="./static/scholarships2.jpg" fluid/> 
             <b-img class="p-2" center src="./static/scholarships3.jpg" fluid/> 
-            <b-col>
-            <!-- <p class="text-lg-left">Motor City Dance Productions, Inc.<br/>
-            245 8th Ave, #232<br/>
-            New York, NY 10011
-            </p> -->
-            <p class="p-5">Questions? <a v-bind:href="`mailto:${emailinfo}`">{{nameinfo}}</a> us or call 917-687-4811<br/>
+            </b-row>
+            <b-row class="p-3 pt-5">
+            <p class="text-lg-left">Motor City Tap Fest is proud to continue offering scholarships to aspiring dancers and dance educators that need financial help to continue growing and working toward new goals. Questions? <a v-bind:href="`mailto:${emailinfo}`">{{nameinfo}}</a> us or call 917-687-4811<br/>
             </p>
-        </b-col>
-      </b-row>
-      <b-row class="p-3 pt-5">
+        </b-row>
+      <b-row class="p-3 pt-3">
           <h2>PAST RECIPIENTS</h2>
       </b-row>
-      <b-row class="p-3 pt-5">
+      <b-row class="p-3 pt-2">
           <b-col lg="4">
           <div class="past">
           <b-img class="p-2" src="./static/scholarship-recipient_kalysta.jpg" fluid/>
