1d86451879cd0f0b027c8e59252350892575445a
Jaffa Aharonov <jaharonov@gmail.com>
2019-03-07 10:18:45 -0600

    added mcsoles ticket info

--- src/components/McSoles.vue
+++ src/components/McSoles.vue
@@ -6,10 +6,13 @@
           <h1><strong>motor city soles</strong></h1>
       </b-row>
       <b-row class="pt-4 pl-3 text-justify">
+        <b-col lg="6">
           <p>Motor City Soles will be at the historic <a href="https://www.royaloakmusictheatre.com/" target="_blank">Royal Oak Music Theatre</a> on August 10!</p>
          <p> Motor City Soles is a high energy evening of fun for the whole family, featuring leading tap dance arts and tap companies in the industry! More info on 2019's Motor City Soles coming soon!</p>
-      </b-row>
-      
+         <p>Click <a href="https://www.axs.com/events/371283/motor-city-soles-tickets" target="_blank">here</a> to buy tickets!</p>
+         </b-col>
+         <b-col lg="6"><b-img src="./static/motor-city-soles.png" fluid/></b-col>
+      </b-row>   
       <b-row p-5></b-row>
 
   </b-container>
