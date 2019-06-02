320ddb15ae67ebcf8c22eb56bc300efb2149eba9
Jaffa Aharonov <j.aharonov@gmail.com>
2019-06-02 10:35:28 -0500

    switched to 10 percent off

--- src/components/SchedReg.vue
+++ src/components/SchedReg.vue
@@ -8,7 +8,7 @@
 </b-row>
 <b-row class="justify-content-md-center pl-3">
   <p>Registration is now open! Please click <a href="https://motor-city-tap-fest.myshopify.com/collections/all" target="_blank">here</a> or on a package/ticket/etc. below to register!
-  Early bird discounts: 15% before June 1st (use code EARLY15 at checkout!), 10% before July 1st. Class sizes
+  Early bird discounts: 10% before July 1st (use code EARLY10 at checkout!). Class sizes
   are limited!
   </p>
   <p>Download a pdf of the schedule and registration form here: <a href ="./static/2019 MCTF Schedule and Registration.pdf">Schedule and Registration</a></p>
