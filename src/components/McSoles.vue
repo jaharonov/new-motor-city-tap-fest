d9732c3d15eeca14f91f61450f2a60343517c5fc
Jaffa Aharonov <j.aharonov@gmail.com>
2019-07-29 17:46:43 -0500

    switched photos motor city soles

--- src/components/McSoles.vue
+++ src/components/McSoles.vue
@@ -3,15 +3,15 @@
     <!-- <div class="m-5"> -->
       <b-container class="p-2">
       <b-row class="pt-5">
-          <h1><strong>motor city soles</strong></h1>
+          <h1 class="pl-4"><strong>motor city soles</strong></h1>
       </b-row>
       <b-row class="pt-4 pl-3 text-justify">
         <b-col lg="6">
           <p>Motor City Soles will be at the historic <a href="https://www.royaloakmusictheatre.com/" target="_blank">Royal Oak Music Theatre</a> on August 10!</p>
-         <p> Motor City Soles is a high energy evening of fun for the whole family, featuring leading tap dance arts and tap companies in the industry! More info on 2019's Motor City Soles coming soon!</p>
+         <p> Motor City Soles is a high energy evening of fun for the whole family, featuring leading tap dance arts and tap companies in the industry!</p>
          <p>Click <a href="https://www.axs.com/events/371283/motor-city-soles-tickets" target="_blank">here</a> to buy tickets!</p>
          </b-col>
-         <b-col lg="6"><b-img src="./static/motor-city-soles.png" fluid/></b-col>
+         <b-col lg="6"><b-img src="./static/motor-city-soles-new-romt.png" fluid/></b-col>
       </b-row>   
       <b-row p-5></b-row>
 
